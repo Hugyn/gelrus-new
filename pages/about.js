8e32fbc389e3dd602460109bee8a35bdfab93eda
Hugyn <hugoncc@hotmail.com>
2021-12-09 15:18:32 +0000

    e

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/router';
 import DrawerComponent from '../components/DrawerComponent';
 
-function gelrusWork() {
+function GelrusWork() {
     const router = useRouter()
     return (
         <DrawerComponent animate={{height:'100%'}} title="About Gelru's" >
@@ -11,4 +11,4 @@ function gelrusWork() {
     )
 }
 
-export default gelrusWork;
+export default GelrusWork;
